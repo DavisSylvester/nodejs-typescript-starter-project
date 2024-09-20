370ca77b30393bb75e22942e0b9a0e70098a567f
Davis Sylvester <davis@sylvesterllc.com>
2024-09-20 08:19:26 -0500

    more changes

---
+++ src/helper/clone.ts
@@ -0,0 +1,20 @@
+import { spawn } from "child_process";
+
+export const cloneGitRepo = async (url: string) => {
+
+	console.log('Cloning Git Repo...');
+
+	await new Promise((resolve, reject) => {
+
+		const child = spawn('git', ['clone', url], { stdio: 'inherit' });
+
+		child.on('exit', (code) => {
+			if (code === 0) {
+				resolve(code);
+			} else {
+				reject(code);
+			}
+		});
+
+});
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+#!/usr/bin/env node
 import chalkAnimation from 'chalk-animation';
-
 import { parseArgs } from './helper/args-helper.js';
 import { dependencyInstaller } from "./helper/depInstaller.mjs";
 import { getProjectName } from './prompts/menu-prompts.js';
 import { NodeJsLibrary } from './projects/NodeJsLibrary.js';
+import { cloneGitRepo } from './helper/clone.js';
 
 
 // export const add = (a: number, b: number) => {
@@ -17,7 +18,9 @@ import { NodeJsLibrary } from './projects/NodeJsLibrary.js';
 // console.log('Answer 1: ', answer);
 
 
+const templateRepo = "https://github.com/DavisSylvester/project-template-repo.git"
 
+await cloneGitRepo(templateRepo)
 //
 // const result = await getProjectName();
 const n = new NodeJsLibrary();
